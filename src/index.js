cc294fec4ecc7f03ff0b023842c781c0245d6c15
ilyansoul <ilyan.kortbi.ifcha@gmail.com>
2023-03-13 17:59:01 +0200

    second

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import App from './App';
 import { BrowserRouter} from 'react-router-dom';
 import './components/styles/HomePage.css';
 import 'semantic-ui-css/semantic.min.css'
-import  { disabledReactDevTools } from '@fvilers/disable-react-devtools'
 
-if (ProcessingInstruction.env.NODE_ENV === 'production') disabledReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
