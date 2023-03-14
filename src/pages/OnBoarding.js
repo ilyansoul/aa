e3c62927f0f19a96f662cdd1331e6da7ffe337e9
ilyansoul <ilyan.kortbi.ifcha@gmail.com>
2023-03-14 15:05:41 +0200

    seconff

--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -33,7 +33,7 @@ const OnBoarding = () => {
         console.log('submitted')
         e.preventDefault()
         try {
-            const response = await axios.put('http://localhost:5000/user', {formData})
+            const response = await axios.put('/user', {formData})
             console.log(response)
             const success = response.status === 200
             console.log(success);
